0550e081e2835c1558596d815662d34020be9ee1
yingmark2014 <MarkYing2014@gmail.com>
2019-01-12 15:47:53 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int numbera = 1;
+        int numberb = 5;
+        int total;
+
+        total = numbera + numberb;
+
+        System.out.println(total);
+
+    }
+}
